504d63765e8662bdbd202ba1a076fa6ba785922a
roche <email": null>
2016-02-11 19:54:42 -0800

    Adds seed file

--- app/controllers/followers.rb
+++ app/controllers/followers.rb
@@ -1,4 +1,4 @@
-post '/users/:user_id/followers/:id'
+post '/users/:user_id/followers/:id' do
   user = User.find(params[:user_id])
   following = User.find(params[:id])
 
@@ -8,13 +8,13 @@ post '/users/:user_id/followers/:id'
   redirect '/users/user_id/followers'
 end
 
-get '/users/:user_id/following'
+get '/users/:user_id/following' do
   @user = User.find(params[:user_id])
 
   erb :'views/following'
 end
 
-get '/user/:user_id/followers'
+get '/user/:user_id/followers' do
   @user = User.find(params[:user_id])
 
   erb :'views/followers'
