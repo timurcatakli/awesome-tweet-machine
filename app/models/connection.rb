f34071588df32f1ab1dacc3bd7927bed1496e1af
Peter Lowe <peter.lowe42@gmail.com>
2016-02-12 12:36:39 -0800

    Validate follower following uniqueness

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,6 +1,9 @@
 class Connection < ActiveRecord::Base
   belongs_to :follower,  class_name: 'User'
-  belongs_to :followed, class_name: 'User'  #class name users foreign key conneciton ID
+  belongs_to :followed, class_name: 'User'
+
+  # User can only follow another user once
+  validates :followed_id, uniqueness: { scope: :follower_id }
 
   # Remember to create a migration!
 end
