e43ddb5b0e91d98668e56efdee0f3321a9cd6ac7
Lindsey Ullman <lindsey.ullman@gmail.com>
2016-02-12 15:04:59 -0800

    Update seed file to populate retweets

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,37 +8,50 @@ require 'faker'
 require_relative '../app/models/user'
 require_relative '../app/models/tweet'
 
-13.times do
+20.times do
   name = Faker::Name.name
   user = User.create( name: name,
                 username: Faker::Internet.user_name(name),
                 email: Faker::Internet.safe_email(name)
                 )
-  tweet = Tweet.create(post: Faker::Lorem.sentence(4))
-  user.tweets << tweet
+  rand(10).times do
+    tweet = Tweet.create(post: Faker::Lorem.sentence(4))
+    user.tweets << tweet
+    tweet.save
+  end
+  user.password = "test"
   user.save
-  tweet.save
 end
 
-13.times do
-  Tweet.create(post: Faker::Lorem.sentence(4))
-end
+10.times do
+  random_user = User.all.sample(1)[0].id
 
-13.times do
-  tweet = Tweet.find(rand(1..13))
-  user = User.find(rand(1..13))
-  user.tweets << tweet
-  user.save
-  tweet.save
+  original_tweet = Tweet.all.sample(1)[0]
+  p "Tweet: "
+  p original_tweet
+  Retweet.create!(tweet_id: original_tweet.id, user_id: random_user) if original_tweet.user_id != random_user
 end
 
 26.times do
   user1 = User.find(rand(1..13))
   user2 = User.find(rand(1..13))
 
-  user1.followers << user2
+  user1.followers << user2 if user1 != user2
 
   user1.save
   user2.save
 end
+
+
+
+26.times do
+  user1 = User.find(rand(1..13))
+  user2 = User.find(rand(1..13))
+
+  user2.followers << user1 if user1 != user2
+
+  user1.save
+  user2.save
+end
+
 
